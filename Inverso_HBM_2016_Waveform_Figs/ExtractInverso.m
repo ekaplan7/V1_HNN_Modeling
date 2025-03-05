31470fde1787b5b953778d6e7c8b7220737f7dc5
Liz Kaplan <likaplan@ucsd.edu>
2025-03-05 11:26:44 -0800

    add data

--- Inverso_HBM_2016_Waveform_Figs/ExtractInverso.m
+++ Inverso_HBM_2016_Waveform_Figs/ExtractInverso.m
@@ -10,26 +10,29 @@ ydata = get(a, 'YData');
 time_variable = xdata{4,1};
 %first line is presentation in the upper hemisphere
 %
-y_data1 = ydata{8,1};
-y_data2 = ydata{4,1};
+y_data1 = ydata{10,1};
+y_data2 = ydata{6,1};
 
-plot(time_variable, y_data1);
+plot(time_variable, y_data2);
+
 
 averaged_data = ((y_data2 + y_data1) / 2);
+averaged_data
 
 %% interpolate data 
-
-averaged_data_resampled = resample(double(averaged_data), 600, 213, 0);
+class(averaged_data)
+size(averaged_data)
+averaged_data = double(averaged_data(:));
+averaged_data_resampled = resample(averaged_data, 600, 213);
 averaged_data_resampled = averaged_data_resampled(1:171);
-
+ver signal
 %create time vector
 
-tvec_resampled = resample(double(x_variable), 600, 213, 0);
+tvec_resampled = resample(double(time_variable), 600, 213, 0);
 tvec_resampled = tvec_resampled(1:171);
 
 %check figure to make sure it is correct 
-plot(tvec_resampled, averaged_data_resampled);
-
+plot(time_variable, averaged_data_resampled);
 squeeze(tvec_resampled);
 tvec_resampled = tvec_resampled.';
 squeeze(averaged_data_resampled);
@@ -38,5 +41,5 @@ averaged_data_resampled = averaged_data_resampled.';
 % export as a txt file 
 hnn_file = [tvec_resampled, averaged_data_resampled];
 hnn_file_numeric = hnn_file;
-dlmwrite(['Inverso_Data.txt'],hnn_file_numeric,'delimiter',' ')
+dlmwrite(['Inverso_Data_New.txt'],hnn_file_numeric,'delimiter',' ')
 
